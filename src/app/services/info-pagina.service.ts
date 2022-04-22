3dde875791a3c4c2222c5a546ba8f6a78106415f
Victor Hugo <vrmq@gft.com>
2022-04-22 15:46:48 -0500

    add link to firebase and implementations into htmkl templates are done

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,30 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada: boolean = false;
+  equipo: any[] = [];
+  urlBase = 'https://portfolio-app-9fbd5-default-rtdb.firebaseio.com/equipo.json';
 
   constructor( private http: HttpClient ) {
-    console.log('Servicio de pagaina listo');
 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     //leer el json
     this.http.get('assets/data/data-page.json')
       .subscribe( ( resp: InfoPagina ) => {
         this.cargada = true;
         this.info = resp;
         console.log( resp );
+      })
+  }
+
+  private cargarEquipo() {
+    this.http.get(this.urlBase)
+      .subscribe( (resp: any ) => {
+        this.equipo = resp;
+        console.log(resp);
       })
   }
 }
